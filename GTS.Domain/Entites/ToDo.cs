1726eb3a8413d586ee94969fc24277f77e63358c
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-05-07 19:48:21 +0300

    Add ToDo class

---
+++ GTS.Domain/Entites/ToDo.cs
@@ -0,0 +1,11 @@
+﻿using GTS.ToDo.Domain.Common;
+
+namespace GTS.ToDo.Domain.Entites
+{
+    public class ToDo : BaseAuditableEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
