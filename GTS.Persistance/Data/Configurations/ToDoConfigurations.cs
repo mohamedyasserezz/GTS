7c93034fd03341f7d5a6d50b8715a2de915b6381
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-05-07 20:37:09 +0300

    Add Configuration for ToDo

---
+++ GTS.Persistance/Data/Configurations/ToDoConfigurations.cs
@@ -0,0 +1,39 @@
+﻿using GTS.TaskManagement.Domain.Common;
+using GTS.TaskManagement.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTS.Persistance.Data.Configurations
+{
+    public class ToDoConfigurations : IEntityTypeConfiguration<ToDo>
+    {
+        public void Configure(EntityTypeBuilder<ToDo> builder)
+        {
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(x => x.Priority)
+                .HasConversion(
+
+                    T => T.ToString(),
+                    t => (Priority)System.Enum.Parse(typeof(Priority), t)
+                );
+
+            builder.Property(x => x.Status)
+                .HasConversion(
+
+                    T => T.ToString(),
+                    t => (Status)System.Enum.Parse(typeof(Status), t)
+                );
+
+
+
+        }
+    }
+
+}
