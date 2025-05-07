8a146a6ca3465b567386c355f70a2fa684eef255
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-05-07 20:38:29 +0300

    Add missing attributes

--- GTS.Domain/Entites/ToDo.cs
+++ GTS.Domain/Entites/ToDo.cs
@@ -1,11 +1,17 @@
-﻿using GTS.ToDo.Domain.Common;
+﻿using GTS.TaskManagement.Domain.Common;
 
-namespace GTS.ToDo.Domain.Entites
+namespace GTS.TaskManagement.Domain.Entites
 {
     public class ToDo : BaseAuditableEntity
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Title { get; set; } = string.Empty;
         public string? Description { get; set; }
+
+        public Priority Priority { get; set; }
+
+        public Status Status { get; set; }
+
+        public DateTime? DueDate { get; set; } 
     }
 }
