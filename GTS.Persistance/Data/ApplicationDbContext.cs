e25b89ac1b088258e59ca357bc213fa863292f23
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-05-07 20:36:32 +0300

    Add DbContext

---
+++ GTS.Persistance/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using GTS.TaskManagement.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace GTS.TaskManagement.Persistance.Data
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+
+        public DbSet<ToDo> MyProperty { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
