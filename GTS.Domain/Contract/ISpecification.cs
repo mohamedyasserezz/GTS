b93bf8377e8809f8e07c15ffe7b1c8b6e0d7bec8
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-05-07 21:07:29 +0300

    Add ISpecification

---
+++ GTS.Domain/Contract/ISpecification.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace GTS.Domain.Contract
+{
+    public interface ISpecification<TEntity>
+    {
+        public Expression<Func<TEntity, bool>>? Criteria { get; set; }
+        public List<Expression<Func<TEntity, object>>> Includes { get; set; }
+        public Expression<Func<TEntity, object>>? OrderBy { get; set; }
+        public Expression<Func<TEntity, object>>? OrderByDesc { get; set; }
+        public int Take { get; set; }
+        public int Skip { get; set; }
+        public bool IsPaginationEnabled { get; set; }
+    }
+}
