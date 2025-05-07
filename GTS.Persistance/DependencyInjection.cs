6acfb34a792f39d24cb9cfe844728c26141875a5
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-05-07 20:39:03 +0300

    register ApplicationDbContext in DI

---
+++ GTS.Persistance/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using GTS.TaskManagement.Persistance.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using GTS.TaskManagement.Persistance.Data.Interceptor;
+namespace GTS.TaskManagement.Persistance
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>((serviceProvider, optionsBuilder) =>
+            {
+                optionsBuilder
+                .UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+                .AddInterceptors(serviceProvider.GetRequiredService<AuditInterceptor>());
+            });
+
+            return services;
+        }
+    }
+}
